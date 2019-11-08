4223c826787d6bc9a3b6a4153b05841abaf6165c
Lefroel <gomanvf8@gmail.com>
2019-11-08 21:09:40 +0700

    Right token

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import discord as ds
 from discord.ext import commands
 
 
-TOKEN = 'NjQyMTkyMjQ5NTYzOTA2MDUw.XcV0SA.EehXyZwaHBDjfWCET2AbcN8LiHU'
-prefix = '!'
+TOKEN = 'NjQyMTkyMjQ5NTYzOTA2MDUw.XcV05g.6Uqk-PWNpddZ4dvD3eqVATpdAs0'
+prefix = 'e!'
 
 bot = commands.Bot(command_prefix=prefix)
 
 @bot.command(pass_context=True) #allow to return arguments
 async def test(ctx, arg): #make async function of bot
     await ctx.send(arg) #returning argument
+
+
+async def help(ctx):
+    await ctx.send(bot.commands)
 
 
 bot.run(TOKEN)
